4c8f363771b5d28b5661ad23382d7f0c94769557
Petro Vlasov <petrovlasov@laptoptest.ua3>
2016-02-12 20:43:04 +0200

    KZ3

--- sdacademy/urls.py
+++ sdacademy/urls.py
@@ -1,27 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from . import views
-
-#def quadratic(request):
-#    a = request.GET['a']
-#   print a, type(a)
-#    b = request.GET['b']
-#    print b, type(b)
-#    c = request.GET['c']
-#    d = b**2 - 4*a*c
-#    print d
-#    print c, type(c) 
-#    return render_to_response('results.html', {'a':a, 'b':b, 'c':c})
-
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^student_list/$', views.student_list, name='student_list'),
     url(r'^student_detail/$', views.student_detail, name='student_detail'),
-#    url(r'^quadratic/results/$', quadratic, name='quadratic'),
+    url(r'^quadratic/results/$', views.quadratic, name='results'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     # Examples:
--- sdacademy/views.py
+++ sdacademy/views.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.template import Context
 
 
 def index(request):
@@ -12,3 +14,56 @@ def student_detail(request):
     
 def contact(request):
     return render(request, 'contact.html')
+
+def quadratic(request):
+    context={}
+    a = context['a'] = (request.GET['a'])
+    b = context['b'] = (request.GET['b'])
+    c = context['c'] = (request.GET['c'])
+
+    try:
+        a = int(a)
+        if a == 0:
+            context['zeroa'] = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
+    except ValueError:
+        if str(a):
+            context['nonemptya'] = "коэффициент не целое число"
+        else:
+            context['emptya'] = "коэффициент не определен"
+    
+    try:
+        b = int(b)
+    except ValueError:
+        if str(b):
+            context['nonemptyb'] = "коэффициент не целое число"
+        else:
+            context['emptyb'] = "коэффициент не определен"
+
+    try:
+        c = int(c)
+    except ValueError:
+        if str(c):
+            context['nonemptyc'] = "коэффициент не целое число"
+        else:
+            context['emptyc'] = "коэффициент не определен"
+
+    try:
+        context['d'] = float(b)**2 - 4*float(a)*float(c)
+        d = context['d']
+        if d < 0:
+            context['descr'] = 'Дискриминант: %d' %d
+            context['result'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+        elif d == 0 and a != 0:
+            context['descr'] = 'Дискриминант: %d' %d
+            context['x1'] = (-float(b) + float(d) ** (1/2.0)) / 2.0*float(a)
+            context['result'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f" %context['x1']
+        elif d > 0 and a != 0:
+            context['descr'] = 'Дискриминант: %d' %d
+            context['x1'] = (-float(b) + float(d) ** (1/2.0)) / 2*float(a)
+            context['x2'] = (-float(b) - float(d) ** (1/2.0)) / 2*float(a)
+            print context['x2']
+            context['result'] = "Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f" % (context['x1'], context['x2'])       
+    except ValueError:
+        context['error'] = "error"
+
+    return render(request, 'results.html', context)
